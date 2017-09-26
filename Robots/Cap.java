a85d7ab86a861bc26304a9be69237c116cd90680
cmis <cmis@cmis-hs02.(none)>
2017-09-26 14:03:17 +0700

    started robots

---
+++ Robots/Cap.java
@@ -0,0 +1,37 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.awt.Color;
+public class Cap extends Robot
+{
+    public Cap(){
+        super(Color.red);
+    }
+
+    public void init(){
+
+    }
+
+    /**
+     * What your robot can do:
+     * right() => move 1 to the right
+     * left() => move 1 to the left
+     * up() => move 1 up
+     * down() => move 1 down
+     * isClearRight() => true means no block to the right
+     * isClearRight() => true means no block to the left
+     * isClearUp() => true means no block up
+     * isClearDown() => true means no block down
+     */
+    public void behave(){
+        for (int i = 1; i > 0;) {
+            if (isClearRight()) {
+                right();
+            }
+            else if (isClearUp()) {
+                up();
+            }
+            else if (isClearDown()) {
+                down();
+            }
+        }
+    }
+}

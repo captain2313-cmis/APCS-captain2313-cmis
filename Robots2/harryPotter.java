6f1404991a157d2e78717471ffe3a8b88f5402a4
cmis <cmis@cmis-hs02.(none)>
2017-11-13 14:36:31 +0700

    made harrypotter faster

--- Robots2/harryPotter.java
+++ Robots2/harryPotter.java
@@ -77,46 +77,75 @@ public class harryPotter extends Robot
                             down();
                         }
                         else {
-                            setData(1, 1);
-                        }
-                    }
-                    //2.1 return upward and to the right
-                    else {
-                        if (getData(3) == getData(2)) {
-                            if (isClearUp()) {
-                                up();
-                            }
-                            else {
+                            if (getData(3) == getData(2)) {
+                                setData(1, 1);
                                 setData(3, 0);
-                                setData(1, 0);
                                 right();
                             }
+                            else {
+                                setData(4, getX() + 11);
+                                setData(5, getY() + 12);
+                                setData(6, getX() - 1);
+                                setData(0, 3);
+                                setData(1, 0);
+                            }
+                        }
+                    }
+                    //2.1 get up whilst scanning
+                    else if (getData(1) == 1) {
+                        if (isClearUp()) {
+                            setData(3, getData(3) + 1);
+                            up();
                         }
                         else {
+                            if (getData(3) == getData(2)) {
+                                setData(1, 0);
+                                setData(3, 0);
+                                right();
+                            }
+                            else {
+                                setData(1, 2);
+                                setData(3, 0);
+                                setData(2, 12);
+                                left();
+                            }
+                        }
+                    }
+                    //2.2 get from bottom corner to top corner, calculate
+                    else {
+                        if (getData(3) < getData(2)) {
+                            setData(3, getData(3) + 1);
+                            up();
+                        }
+                        else{
                             setData(4, getX() + 11);
                             setData(5, getY() + 12);
                             setData(6, getX() - 1);
                             setData(0, 3);
-                            setData(1, 0);
+                            setData(1,0);
+                            right();
                         }
                     }
                 }
                 else {
                     //3
                     if (getData(0) == 3) {
-                        //3.0 scan top try to get it
+                        //3.0 scan top try to get in
                         if (getData(1) == 0) {
-                            
+
                         }
                     }
                 }
             }
         }
-        //3.1 scan right try to get it
-        //3.2 scan bottom try to get it
-        //3.3 scan left try to get it
+        //3.1 scan right try to get in
+        //3.2 scan bottom try to get in
+        //3.3 scan left try to get in
 
         //4 get in
+        //4.1 top left corner
+        
+        //5 Snake inside room
     }
 
     public static void locateOpening(int x, int y) {
